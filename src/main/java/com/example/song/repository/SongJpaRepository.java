18d8e92d48428d148283ffa445584fec51ab218a
SureshChintapanti <122145536+SureshChintapanti@users.noreply.github.com>
2023-03-11 21:01:08 +0530

    Update SongJpaRepository.java

--- src/main/java/com/example/song/repository/SongJpaRepository.java
+++ src/main/java/com/example/song/repository/SongJpaRepository.java
@@ -1,9 +1,12 @@
+
+
 package com.example.song.repository;
 
-import com.example.song.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.song.model.Song;
+
 @Repository
-public interface SongJpaRepository extends JpaRepository<Song, Integer> {
-}
+public interface SongJpaRepository extends JpaRepository<Song, Integer> {}
+
