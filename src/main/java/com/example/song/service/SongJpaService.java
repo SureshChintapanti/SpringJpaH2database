d2dbef624890c0fa1365260c8a9d783aeba9c89e
SureshChintapanti <122145536+SureshChintapanti@users.noreply.github.com>
2023-03-11 21:02:34 +0530

    Update SongJpaService.java

--- src/main/java/com/example/song/service/SongJpaService.java
+++ src/main/java/com/example/song/service/SongJpaService.java
@@ -1,14 +1,14 @@
 package com.example.song.service;
-
-import com.example.song.model.Song;
-import com.example.song.repository.SongJpaRepository;
-import com.example.song.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.*;
+import com.example.song.repository.SongJpaRepository;
+import com.example.song.repository.SongRepository;
+import com.example.song.model.Song;
 
 @Service
 public class SongJpaService implements SongRepository {
@@ -17,57 +17,64 @@ public class SongJpaService implements SongRepository {
     private SongJpaRepository songJpaRepository;
 
     @Override
-    public ArrayList<Song> getAllSongs(){
-        List<Song> songlist=songJpaRepository.findAll();
-        ArrayList<Song> songs=new ArrayList<>(songlist);
-        return songs;
+    public void deleteSong(int songId) {
+        try {
+            songJpaRepository.deleteById(songId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
     @Override
-    public Song getSongById(int songId) {
-        try{
-            Song song=songJpaRepository.findById(songId).get();
-            return song;
-        } catch (Exception e){
+    public Song updateSong(int songId, Song song) {
+        try {
+            Song existingSong = songJpaRepository.findById(songId).get();
+            if (song.getSongName() != null) {
+                existingSong.setSongName(song.getSongName());
+            }
+            if (song.getLyricist() != null) {
+                existingSong.setLyricist(song.getLyricist());
+            }
+            if (song.getSinger() != null) {
+                existingSong.setSinger(song.getSinger());
+            }
+            if (song.getMusicDirector() != null) {
+                existingSong.setMusicDirector(song.getMusicDirector());
+            }
+            
+            songJpaRepository.save(existingSong);
+            return existingSong;
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
 
     @Override
     public Song addSong(Song song) {
-        songJpaRepository.save(song);
-        return song;
-    }
-
-    @Override
-    public Song updateSong(int songId, Song song) {
-        try{
-            Song newSong=songJpaRepository.findById(songId).get();
-            if(song.getSongName()!=null){
-                newSong.setSongName(song.getSongName());
-            }
-            if(song.getLyricist()!=null){
-                newSong.setLyricist(song.getLyricist());
-            }
-            if(song.getMusicDirector()!=null){
-                newSong.setMusicDirector(song.getMusicDirector());
-            }
-            if(song.getSinger()!=null){
-                newSong.setSinger(song.getSinger());
-            }
-            songJpaRepository.save(newSong);
-            return newSong;
-        }catch (Exception e){
+        try {
+            songJpaRepository.save(song);
+            return song;
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
 
     @Override
-    public void deleteSong(int songId) {
-        try{
-            songJpaRepository.deleteById(songId);
-        }catch (Exception e){
+    public Song getSongById(int songId) {
+        try {
+            Song song = songJpaRepository.findById(songId).get();
+            return song;
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
+
+    @Override
+    public ArrayList<Song> getSongs() {
+        List<Song> songList = songJpaRepository.findAll();
+        ArrayList<Song> songs = new ArrayList<>(songList);
+
+        return songs;
+    }
+
 }
