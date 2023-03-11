0e2b5a1ccf7250edf9a1ab03fc16c83ba02f0667
SureshChintapanti <122145536+SureshChintapanti@users.noreply.github.com>
2023-03-11 21:01:40 +0530

    Update SongRepository.java

--- src/main/java/com/example/song/repository/SongRepository.java
+++ src/main/java/com/example/song/repository/SongRepository.java
@@ -1,18 +1,20 @@
 package com.example.song.repository;
-
 import com.example.song.model.Song;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public interface SongRepository {
 
-    ArrayList<Song> getAllSongs();
+  void deleteSong(int songId);
 
-    Song getSongById(int songId);
+  Song updateSong(int songId, Song song);
 
-    Song addSong(Song song);
+  Song addSong(Song song);
 
-    Song updateSong(int songId, Song song);
+  Song getSongById(int songId);
 
-    void deleteSong(int songId);
+  ArrayList<Song> getSongs();
 }
+
+
+
